9aa3cfd2a32c60b61e60713291f6e3f9b20fef22
Nicholas Sosa <nicksosa456@gmail.com>
2019-07-31 13:59:55 -0500

    working search bar

--- js/nickTestJS.js
+++ js/nickTestJS.js
@@ -1,13 +1,32 @@
-$.ajax ({
-    type: 'GET',
-    dataType: 'jsonp',
-    crossDomain: true,
-    jsonp: 'json_callback',
-    url: `http://www.giantbomb.com/api/search/?format=jsonp&api_key=${giantBombKey}&query=batman`
-}).done(function(data) {
-    console.log(data);
-}).fail(function() {
-    alert("error");
-}).always(function() {
-    // alert("complete");
+"use strict";
+const searchFunc = $('#searchGames').click(function () {
+    let search = $('#gameSearch').val();
+    console.log(search);
+    $.ajax ({
+        type: 'GET',
+        dataType: 'jsonp',
+        crossDomain: true,
+        jsonp: 'json_callback',
+        url: 'http://www.giantbomb.com/api/search/?format=jsonp&api_key='+giantBombKey+'&query='+search
+    }).done(function(data) {
+        console.log(data);
+    }).fail(function() {
+        // alert("error");
+    }).always(function() {
+        // alert("complete");
+    });
 });
+
+// $.ajax ({
+//     type: 'GET',
+//     dataType: 'jsonp',
+//     crossDomain: true,
+//     jsonp: 'json_callback',
+//     url: 'http://www.giantbomb.com/api/search/?format=jsonp&api_key='+giantBombKey+'&query=batman'
+// }).done(function(data) {
+//     console.log(data);
+// }).fail(function() {
+//     // alert("error");
+// }).always(function() {
+//     // alert("complete");
+// });
