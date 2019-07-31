453cf2db2c13a5e8b1bf3b026a3a4b42a408b8d4
Nicholas Sosa <nicksosa456@gmail.com>
2019-07-31 11:51:41 -0500

    added fire attack

--- src/AdventureGame.java
+++ src/AdventureGame.java
@@ -6,6 +6,9 @@ public class AdventureGame {
         String charaName;
         int heroHP;
         int heroATT;
+        int heroMP = 3;
+        int fireATT = 6;
+        int fireMP = 3;
         int potion;
         int totalPotions = 4;
         String enemy = "Wolf";
@@ -26,10 +29,10 @@ public class AdventureGame {
             enemyATT = 2;
             do {
                 System.out.println("Stats: ");
-                System.out.printf("-- %-5s-- | -- %-5s--\n", charaName, enemy);
-                System.out.printf("HP: %-6d | Enemy HP: %-5d\n\n", heroHP, enemyHP);
+                System.out.printf("--  %-6s-- | -- %-5s--\n", charaName, enemy);
+                System.out.printf("HP: %d MP: %d | Enemy HP: %-5d\n\n", heroHP, heroMP, enemyHP);
                 System.out.println("Actions: ");
-                System.out.printf("Attack || Drink Potion(%d) || Run\n", totalPotions);
+                System.out.printf("Attack || Drink Potion(%d) || Fire Magic(%dMP)|| Run\n", totalPotions, fireMP);
                 String action = scanner.nextLine();
                 if (action.equalsIgnoreCase("attack")) {
                     System.out.println("You did damage.\n");
@@ -40,6 +43,13 @@ public class AdventureGame {
                     heroHP += potion;
                     totalPotions --;
                     heroHP -= enemyATT;
+                } else if (heroMP==0 && action.toLowerCase().contains("fire")) {
+                    System.out.println("You ran out of MP! Try something else!");
+                } else if (action.toLowerCase().contains("fire")) {
+                    System.out.println("You set it ablaze!");
+                    enemyHP -= fireATT;
+                    heroHP -= enemyATT;
+                    heroMP -= fireMP;
                 } else if (action.equalsIgnoreCase("run")) {
                     System.out.println("You successfully ran away.");
                     break;
