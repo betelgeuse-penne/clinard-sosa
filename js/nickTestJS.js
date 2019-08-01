b35c9009f9b3ef8c084283a32a314ab88728982d
Nicholas Sosa <nicksosa456@gmail.com>
2019-08-01 14:15:32 -0500

    trying to get game specfic data with next ajax call.

--- js/nickTestJS.js
+++ js/nickTestJS.js
@@ -1,4 +1,5 @@
 "use strict";
+let gameGuid;
 const searchFunc = $('#searchGames').click(function () {
     let search = $('#gameSearch').val();
     console.log(search);
@@ -8,13 +9,22 @@ const searchFunc = $('#searchGames').click(function () {
         crossDomain: true,
         jsonp: 'json_callback',
         url: 'http://www.giantbomb.com/api/search/?format=jsonp&api_key='+giantBombKey+'&query='+search
+    }).then(function(data) {
+        console.log(data.results[0].guid);
+        gameGuid = data.results[0].guid;
+    }).then($.ajax ({
+        type: 'GET',
+        dataType: 'jsonp',
+        crossDomain: true,
+        jsonp: 'json_callback',
+        url: 'http://www.giantbomb.com/api/game/'+gameGuid+'/?format=jsonp&api_key='+giantBombKey
     }).done(function(data) {
         console.log(data);
     }).fail(function() {
         // alert("error");
     }).always(function() {
         // alert("complete");
-    });
+    }));
 });
 
 // $.ajax ({
