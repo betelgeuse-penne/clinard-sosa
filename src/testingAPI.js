28def226eeb9ebcae20f89a9035838bfa4f71620
Brandi <brandiclinard021911@gmail.com>
2019-08-01 15:21:28 -0500

    Query search functioning for mediawiki api

--- src/testingAPI.js
+++ src/testingAPI.js
@@ -1,9 +1,61 @@
 //Wiki API
 
-const searchItemFirstWord= "Bill";
-const searchItemSecondWord = "Nye";
-const wikiURL = `https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${searchItemFirstWord}%20${searchItemSecondWord}&utf8=&format=json`;
+// const searchItemFirstWord= "Bill";
+// const searchItemSecondWord = "Nye";
+// const wikiURL = `https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${searchItemFirstWord}%20${searchItemSecondWord}&utf8=&format=json`;
 
-fetch(wikiURL).then(response => response.json()).then(data => console.log(data));
+// fetch(wikiURL).then(response => response.json()).then(data => console.log(data));
+
+// $.ajax ({
+//     type: 'GET',
+//     dataType: 'jsonp',
+//     crossDomain: true,
+//     jsonp: 'json_callback',
+//     url: wikiURL
+// }).done(function(data) {
+//     console.log(data);
+// }).fail(function() {
+//     alert("error");
+// }).always(function() {
+//     // alert("complete");
+// });
+
+// var apiEndpoint = "https://en.wikipedia.org/w/api.php";
+// var params = "?action=query&format=json&list=search&srsearch=apple";
+//
+// fetch(apiEndpoint + params).then(response => response.json()).then(data => console.log(data));
+
+
+// /**
+//  * The function to wrap the result
+//  */
+// var callback = function (response) {
+//     var pages = response.query.random; // Process the output to get the titles
+//     Object.keys(pages).forEach(function(key) {
+//         console.log(pages[key].title);
+//     });
+// };
+//
+// var scriptTag = document.createElement("script"); // Dynamically create a "script" tag
+// scriptTag.src = apiEndpoint + "?" + params + "&callback=callback"; // Point to the query string
+//
+// document.body.appendChild(scriptTag); // Add the script tag to the document
+
+/*THIS PROVIDES TITLES AND SNIPPETS*/
+
+$.ajax({
+    url: '//en.wikipedia.org/w/api.php',
+    data: {
+        action: 'query',
+        list: 'search',
+        srsearch: 'Richard Feynman',
+        format: 'json',
+        formatversion: 2
+    },
+    dataType: 'jsonp',
+    success: function (x) {
+        console.log( x.query.search);
+    }
+});
 
 console.log("it works.");
