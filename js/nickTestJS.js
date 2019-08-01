d36a0778fdc3058fe668a389ec8c66628d090df4
Nicholas Sosa <nicksosa456@gmail.com>
2019-08-01 16:46:39 -0500

    displaying cover art and game name on a search

--- js/nickTestJS.js
+++ js/nickTestJS.js
@@ -11,6 +11,18 @@ const searchFunc = $('#searchGames').click(function () {
         url: 'http://www.giantbomb.com/api/search/?format=jsonp&api_key='+giantBombKey+'&query='+search+'&resources=game'
     }).done(function(data) {
         console.log(data);
+        $('#displayGames').html("");
+        let imageURL;
+        let html = "";
+        html += " ";
+        for (let i = 0; i <= data.results.length-1; i++) {
+            imageURL = data.results[i].image.original_url;
+            // $('#displayGames').append(`<div class="col-3"><img src="${imageURL}" alt="gameImage"><h4>${data.results[i].name}</h4></div>`);
+            html += `<div class="col-4"><img class="gameImg" src="${imageURL}" alt="gameImage"><h5>${data.results[i].name}</h5></div>`;
+            // html += `<img src="${imageURL}" alt="gameImage">`;
+            // console.log(data.results[i].image.medium_url);
+        }
+        $(html).appendTo('#displayGames');
         gameGuid = data.results[0].guid;
         console.log(gameGuid);
     }).then(gameInfo);
