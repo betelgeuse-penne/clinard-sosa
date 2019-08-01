e8d8e25bffff4d213d535f435f655f51dd694abd
Nicholas Sosa <nicksosa456@gmail.com>
2019-08-01 18:01:38 -0500

    working modals on image click!

--- js/nickTestJS.js
+++ js/nickTestJS.js
@@ -11,20 +11,90 @@ const searchFunc = $('#searchGames').click(function () {
         url: 'http://www.giantbomb.com/api/search/?format=jsonp&api_key='+giantBombKey+'&query='+search+'&resources=game'
     }).done(function(data) {
         console.log(data);
+
         $('#displayGames').html("");
         let imageURL;
         let html = "";
         html += " ";
         for (let i = 0; i <= data.results.length-1; i++) {
             imageURL = data.results[i].image.original_url;
-            // $('#displayGames').append(`<div class="col-3"><img src="${imageURL}" alt="gameImage"><h4>${data.results[i].name}</h4></div>`);
-            html += `<div class="col-4"><img class="gameImg" src="${imageURL}" alt="gameImage"><h5>${data.results[i].name}</h5></div>`;
-            // html += `<img src="${imageURL}" alt="gameImage">`;
-            // console.log(data.results[i].image.medium_url);
+            html += `<div class="col-4 gameData center-block align-self-center"><a href="#" id="game-${[i]}" data-toggle="modal" data-target="#gameModal"><img class="gameImg" src="${imageURL}" alt="gameImage"></a><h5>${data.results[i].name}</h5></div>`
         }
+
         $(html).appendTo('#displayGames');
         gameGuid = data.results[0].guid;
-        console.log(gameGuid);
+
+        // Adds Modal Functionality to the images //
+        $('#game-0').on("click", function () {
+            $('#modal-body').empty();
+            $('#gameModalTitle').empty();
+            $('#modal-body').html(`${data.results[0].deck}`);
+            $('#gameModalTitle').html(`${data.results[0].name}`)
+        });
+
+        $('#game-1').on("click", function () {
+            $('#modal-body').empty();
+            $('#gameModalTitle').empty();
+            $('#modal-body').html(`${data.results[1].deck}`);
+            $('#gameModalTitle').html(`${data.results[1].name}`)
+        });
+
+        $('#game-2').on("click", function () {
+            $('#modal-body').empty();
+            $('#gameModalTitle').empty();
+            $('#modal-body').html(`${data.results[2].deck}`);
+            $('#gameModalTitle').html(`${data.results[2].name}`)
+        });
+
+        $('#game-3').on("click", function () {
+            $('#modal-body').empty();
+            $('#gameModalTitle').empty();
+            $('#modal-body').html(`${data.results[3].deck}`);
+            $('#gameModalTitle').html(`${data.results[3].name}`)
+        });
+
+        $('#game-4').on("click", function () {
+            $('#modal-body').empty();
+            $('#gameModalTitle').empty();
+            $('#modal-body').html(`${data.results[4].deck}`);
+            $('#gameModalTitle').html(`${data.results[4].name}`)
+        });
+
+        $('#game-5').on("click", function () {
+            $('#modal-body').empty();
+            $('#gameModalTitle').empty();
+            $('#modal-body').html(`${data.results[5].deck}`);
+            $('#gameModalTitle').html(`${data.results[5].name}`)
+        });
+
+        $('#game-6').on("click", function () {
+            $('#modal-body').empty();
+            $('#gameModalTitle').empty();
+            $('#modal-body').html(`${data.results[6].deck}`);
+            $('#gameModalTitle').html(`${data.results[6].name}`)
+        });
+
+        $('#game-7').on("click", function () {
+            $('#modal-body').empty();
+            $('#gameModalTitle').empty();
+            $('#modal-body').html(`${data.results[7].deck}`);
+            $('#gameModalTitle').html(`${data.results[7].name}`)
+        });
+
+        $('#game-8').on("click", function () {
+            $('#modal-body').empty();
+            $('#gameModalTitle').empty();
+            $('#modal-body').html(`${data.results[8].deck}`);
+            $('#gameModalTitle').html(`${data.results[8].name}`)
+        });
+
+        $('#game-9').on("click", function () {
+            $('#modal-body').empty();
+            $('#gameModalTitle').empty();
+            $('#modal-body').html(`${data.results[9].deck}`);
+            $('#gameModalTitle').html(`${data.results[9].name}`)
+        })
+
     }).then(gameInfo);
 });
 
