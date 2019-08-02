40bb34fda100e1975c731c42612d94880b4edda3
Brandi <brandiclinard021911@gmail.com>
2019-08-02 14:33:23 -0500

    mediawiki display searched information

--- src/mediaWikiAPI.js
+++ src/mediaWikiAPI.js
@@ -1,5 +1,45 @@
 //Wiki API
 
+$(function(){
+
+    $("#wikiSearch").on("click", function (){
+        let searchTerm = $("#searchTerm").val();
+        let wikiURL= "https://en.wikipedia.org/w/api.php?action=opensearch&search=" +searchTerm+"&format=json&callback=?"
+        $.ajax({
+            url: wikiURL,
+            type: 'GET',
+            contentType: "application/json; charset=utf-8",
+            async: false,
+            dataType: "json",
+            success:function(data, status, jqXHR){
+                console.log(data);
+                $("#wikiOutput").html();
+                for(let i=0; i<data[1].length; i++){
+                    $("#wikiOutput").prepend("<div>" +
+                        "<div class='btn'>" +
+                        "<a href =" +
+                        data[3][i] +
+                        "><h2>" +
+                        data[1][i] +
+                        "</h2>" +
+                        "<p>" +
+                        data [2][i] +
+                        "</p></a></div></div>");
+                }
+            },
+        })
+            .done(function(){
+                console.log("success");
+            })
+            .fail(function(){
+                console.log("error");
+            })
+            .always(function(){
+                console.log("complete");
+            });
+    });
+});
+
 // const searchItemFirstWord= "Bill";
 // const searchItemSecondWord = "Nye";
 // const wikiURL = `https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${searchItemFirstWord}%20${searchItemSecondWord}&utf8=&format=json`;
@@ -41,21 +81,22 @@
 //
 // document.body.appendChild(scriptTag); // Add the script tag to the document
 
+
+
 /*THIS PROVIDES FIRST TEN TITLES AND SNIPPETS*/
 
-$.ajax({
-    url: '//en.wikipedia.org/w/api.php',
-    data: {
-        action: 'query',
-        list: 'search',
-        srsearch: 'Batman and robin',
-        format: 'json',
-        formatversion: 2
-    },
-    dataType: 'jsonp',
-    success: function (x) {
-        console.log( x.query.search);
-    }
-});
+// $.ajax({
+//     url: '//en.wikipedia.org/w/api.php',
+//     data: {
+//         action: 'query',
+//         list: 'search',
+//         srsearch: 'Batman and robin',
+//         format: 'json',
+//         formatversion: 2
+//     },
+//     dataType: 'jsonp',
+//     success: function (x) {
+//         console.log( x.query.search);
+//     }
+// });
 
-console.log("it works.");
