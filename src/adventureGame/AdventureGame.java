e3ade73a369549017f1ab9b14f82d12718cd085d
Nicholas Sosa <nicksosa456@gmail.com>
2019-08-05 09:59:07 -0500

    created random generator for enemies

--- src/adventureGame/AdventureGame.java
+++ src/adventureGame/AdventureGame.java
@@ -10,7 +10,20 @@ public class AdventureGame{
 //        System.out.printf("You picked: %s", charaClass);
 
         PlayerCharacter warrior = new WarriorClass("Warrior", 25, 0, 4);
-        Enemy wolf = new Wolf("Wolf", 12, 3);
+        Enemy enemy = new Wolf("wolff", 3, 4);
+
+        int randomEnemy = (int) Math.ceil(Math.random()*50);
+        if (randomEnemy > 0 && randomEnemy <= 10) {
+            enemy = new Wolf("Wolf", 12, 3);
+        } else if (randomEnemy > 10 && randomEnemy <= 20) {
+            enemy = new GiantSpider("Giant Spider", 20, 4);
+        } else if (randomEnemy > 20 && randomEnemy <= 30) {
+            enemy = new Ghoul("Ghoul", 18, 3);
+        } else if (randomEnemy > 30 && randomEnemy <= 48) {
+            enemy = new Goblin("Goblin", 16, 3);
+        } else if (randomEnemy > 48 && randomEnemy <=50) {
+            enemy = new CopperDragon("Copper Dragon", 50, 10);
+        }
 
         String charaName;
         int fireATT = 6;
@@ -24,39 +37,39 @@ public class AdventureGame{
         if (start.equalsIgnoreCase("yes")) {
             System.out.println("Enter your character's name: ");
             charaName = scanner.nextLine();
-            System.out.println(wolf.appears());
+            System.out.println(enemy.appears());
             potion = 10;
             do {
                 System.out.println("Stats: ");
-                System.out.printf("--  %-6s-- | -- %-5s--\n", charaName, wolf.enemyName);
-                System.out.printf("HP: %d MP: %d | Enemy HP: %-5d\n\n", warrior.charaHP, warrior.charaMP, wolf.enemyHP);
+                System.out.printf("--  %-6s-- | -- %-5s--\n", charaName, enemy.enemyName);
+                System.out.printf("HP: %d MP: %d | Enemy HP: %-5d\n\n", warrior.charaHP, warrior.charaMP, enemy.enemyHP);
                 System.out.println("Actions: ");
                 System.out.printf("Attack || Drink Potion(%d) || Magic(%dMP)|| Run\n", totalPotions, fireMP);
                 String action = scanner.nextLine();
                 if (action.equalsIgnoreCase("attack")) {
                     System.out.println(warrior.attack());
-                    System.out.println(wolf.attack());
-                    wolf.enemyHP -= warrior.attackPOW;
-                    warrior.charaHP -= wolf.enemyATT;
+                    System.out.println(enemy.attack());
+                    enemy.enemyHP -= warrior.attackPOW;
+                    warrior.charaHP -= enemy.enemyATT;
                 } else if (action.toLowerCase().contains("potion")) {
                     System.out.println(warrior.drinkPotion());
-                    System.out.println(wolf.attack());
+                    System.out.println(enemy.attack());
                     warrior.charaHP += potion;
                     totalPotions --;
-                    warrior.charaHP -= wolf.enemyATT;
+                    warrior.charaHP -= enemy.enemyATT;
                 } else if (warrior.charaMP==0 && action.toLowerCase().contains("fire")) {
                     System.out.println(warrior.magicATT());
                 } else if (action.toLowerCase().contains("fire")) {
                     System.out.println("You set it ablaze!");
-                    System.out.println(wolf.attack());
-                    wolf.enemyHP -= fireATT;
-                    warrior.charaHP -= wolf.enemyATT;
+                    System.out.println(enemy.attack());
+                    enemy.enemyHP -= fireATT;
+                    warrior.charaHP -= enemy.enemyATT;
                     warrior.charaMP -= fireMP;
                 } else if (action.equalsIgnoreCase("run")) {
                     System.out.println("You successfully ran away.");
                     break;
                 }
-                if (wolf.enemyHP <= 0) {
+                if (enemy.enemyHP <= 0) {
                     System.out.println("You win!");
                     break;
                 }
